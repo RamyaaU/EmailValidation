c7da23dc70c116902796ea6d5ed4c6c83227465c
Ramya.U <ramyayadav2016@gmail.com>
2020-10-31 18:01:58 +0530

    [Ramya] Add . Added UC_3

---
+++ EmailValidation/EmailValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmailCheck
+{
+    public class EmailVaidation
+    {
+        //pattern for email check
+        public static string REGEX_EMAIL = "^[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}$";
+        /// <summary>
+        /// Validates the email.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns></returns>
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+    }
+}
